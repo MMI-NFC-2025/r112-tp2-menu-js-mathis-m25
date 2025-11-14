b5fa8f807b3c98eac995006f911e5a3390477342
mathis-m25 <sautemou@gmail.com>
2025-11-14 02:06:21 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,19 @@ const nav = document.querySelector('.menu');
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
-  // On vérifie d'abord si le menu est actuellement caché
-  const isHidden = nav.getAttribute('aria-hidden') === 'true';  
-  // Mise à jour des attributs ARIA pour accessibilité
-  nav.setAttribute('aria-hidden', !isHidden);
-  toggle.setAttribute('aria-expanded', !isHidden);
+
+  // Agir suivant l'état (actuel) : Lit l'état du bouton AVANT le clic
+  const isOpen = toggle.ariaExpanded === "true"; 
+  const isClosed = !isOpen; // Cette ligne est pour le débug, mais est incluse dans l'énoncé
+
+  // Testez et regardez ce qui s'affiche dans la console
+  console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
+  // En déduire les modifications : On met à jour les attributs en inversant l'état
+  // Le nouvel état 'expanded' est l'inverse de l'ancien 'isOpen'.
+  toggle.setAttribute('aria-expanded', !isOpen);
+  
+  // Le nouvel état 'hidden' est l'inverse de 'expanded', donc égal à l'ancien 'isOpen'.
+  nav.setAttribute('aria-hidden', isOpen); 
+
 });
